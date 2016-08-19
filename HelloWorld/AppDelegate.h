cebd6452d814d5191ab55bdd8b3bd7c60562d0ad
靳杰 <1059828836@qq.com>
2016-08-19 10:15:52 +0800

    upload project

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by baihemiyu001 on 16/8/18.
+//  Copyright © 2016年 zjc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by baihemiyu001 on 16/8/18.
+//  Copyright © 2016年 zjc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
